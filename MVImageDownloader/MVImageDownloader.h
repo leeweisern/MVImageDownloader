89e038b8f3a4e3ba3c01aa34997ce6ab04d60334
kelvin lee wei sern <kelvin@moovby.com>
2019-07-25 10:17:27 +0800

    Initial Commit

---
+++ MVImageDownloader/MVImageDownloader.h
@@ -0,0 +1,19 @@
+//
+//  MVImageDownloader.h
+//  MVImageDownloader
+//
+//  Created by kelvin lee wei sern on 25/07/2019.
+//  Copyright © 2019 MindValley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVImageDownloader.
+FOUNDATION_EXPORT double MVImageDownloaderVersionNumber;
+
+//! Project version string for MVImageDownloader.
+FOUNDATION_EXPORT const unsigned char MVImageDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVImageDownloader/PublicHeader.h>
+
+
